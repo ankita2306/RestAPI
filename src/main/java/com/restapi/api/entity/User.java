5ae758bb01beb7c543833a10535f45c8aebd57dc
Ankita <ankitarajpriya@gmail.com>
2023-06-01 16:52:27 +0530

    Database Running

--- src/main/java/com/restapi/api/entity/User.java
+++ src/main/java/com/restapi/api/entity/User.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 @Table(name = "Users")
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     @Column(name = "firstname")
     private String firstName;
